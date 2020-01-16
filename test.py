56ea1d8fd2f9acbd8b082d72654e3bd19b62069c
kwangmin <rhkdals7362@gmail.com>
2020-01-16 18:52:44 +0900

    BAEKJOON

---
+++ test.py
@@ -0,0 +1,21 @@
+test_case=int(input())
+
+for _ in range(test_case):
+    n,m=list(map(int,input().split(' ')))
+    queue=list(map(int,input().split(' ')))
+    queue=[(i,idx) for idx, i in enumerate(queue)]
+     
+    count=0
+    print(":::::::"+str(max(queue)))
+    """
+    while True:
+        if queue[0][0]==max(queue, key=lambda x:x[0] )[0]:
+            count+=1
+            if queue[0][1]==m:
+                print(count)
+                break
+            else:
+                queue.pop(0)
+        else:
+            queue.append(queue.pop(0))
+    """
