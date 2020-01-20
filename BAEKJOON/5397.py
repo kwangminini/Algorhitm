83bead3497e5d2df885c33562df57a4f3edefc07
kwangmin <rhkdals7362@gmail.com>
2020-01-20 23:45:00 +0900

    BAEKJOON

--- BAEKJOON/5397.py
+++ BAEKJOON/5397.py
@@ -1,1 +1,21 @@
-
+testCase=int(input())
+for i in range(testCase):
+    test=input()
+    testList=[]
+    point=0
+    for j in range(len(test)):
+        if test[j]=='<':
+            if point > 0:
+                point-=1
+        elif test[j]=='>':
+            point+=1
+        elif test[j]=='-':
+            if point-1>0 :
+                del testList[point-1]
+        else :
+            testList.insert(point,test[j])
+            point+=1
+    print(testList)
+    
+                                                                                           
+            
