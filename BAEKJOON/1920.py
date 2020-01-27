6bbf2700176817a871eb93a2a7e11609e6a2fd0f
kwangmin <rhkdals7362@gmail.com>
2020-01-27 23:25:33 +0900

    BAEKJOON normal

---
+++ BAEKJOON/1920.py
@@ -0,0 +1,11 @@
+testCase=int(input())
+a=input().split(" ")
+test=int(input())
+b=input().split(" ")
+aDic={}
+bDic={}
+for i in range(len(a)):
+    aDic[a[i]]=a[i]
+
+for i in range(len(bStr)):
+    
