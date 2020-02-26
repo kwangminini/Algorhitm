b6393a8e889be7216374fc8b8dcdf543ea66e65e
kwangminini <rhkdals7362@gmail.com>
2020-02-27 01:13:45 +0900

    수 정렬하기3 normal

---
+++ BAEKJOON/10989.py
@@ -0,0 +1,10 @@
+import sys
+n=int(sys.stdin.readline())
+array=[0]*10001
+for _ in range(n):
+    num=int(sys.stdin.readline())
+    array[num]+=1
+for i in range(10001):
+    if array[i]!=0:
+        for j in range(array[i]):
+            print(i)
