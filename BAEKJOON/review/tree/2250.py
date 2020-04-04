9186127771a0bf8adb49da621b2510b2fef9a03f
kwangminini <rhkdals7362@gmail.com>
2020-04-04 23:45:35 +0900

    트리의 높이와 너비 hard can't

---
+++ BAEKJOON/review/tree/2250.py
@@ -0,0 +1,8 @@
+class Node :
+    def __init__(self,number,left_node,right_node):
+        self.parent = -1
+        self.number = number
+        self.left_node = left_node
+        self.right_node = right_node
+
+
