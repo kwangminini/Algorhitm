43b3514c69efc0316378d295390f64a8f83930a3
kwangminini <rhkdals7362@gmail.com>
2020-04-11 02:29:48 +0900

    알파벳 normal can't

---
+++ BAEKJOON/backtracking/1987.py
@@ -0,0 +1,31 @@
+"""
+문제 제목 : 알파벳
+문제 난이도 : 중
+문제 유형 : 백트래
+"""
+
+dx=[-1,1,0,0]
+dy=[0,0,-1,1]
+def bfs(x,y):
+    global result
+    q = set()
+    q.add((x,y,array[x][y]))
+    while q:
+        x,y,step = q.pop()
+        result = max(result, len(step))
+
+        for i in range(4):
+            nx = x+dx[i]
+            ny = y+dy[i]
+
+            if(0<=nx and nx<r and ny>=0 and ny<c and array[nx][ny] not in step):
+                q.add((nx,ny,step+array[nx][ny]))
+
+r, c = map(int, input().split())
+array = []
+for _ in range(r):
+    array.append(input())
+
+result = 0
+bfs(0,0)
+print(result)
