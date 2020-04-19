ecbce2720841c20182aeec5d4d568a4a581dd7cb
kwangminini <rhkdals7362@gmail.com>
2020-04-20 02:18:09 +0900

    update code

--- BAEKJOON/codingTest/17389.py
+++ BAEKJOON/codingTest/17389.py
@@ -1,16 +1,15 @@
-# import sys
-# input = sys.stdin.readline
-#
-# n=int(input())
-# result = input()
-# bonus=0
-# resultNum=0
-# for i in range(len(result)):
-#     if result[i] == 'O':
-#         resultNum+=(i+1)+bonus
-#         if result[i+1] == 'O':
-#             bonus+=1
-#         else:
-#             bonus=0
-# print(resultNum)
+import sys
+input = sys.stdin.readline
 
+n=int(input())
+result = input()
+bonus=0
+resultNum=0
+for i in range(len(result)):
+    if result[i] == 'O':
+        resultNum+=(i+1)+bonus
+        if result[i+1] == 'O':
+            bonus+=1
+        else:
+            bonus=0
+print(resultNum)
