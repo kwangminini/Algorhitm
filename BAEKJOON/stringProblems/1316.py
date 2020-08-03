b48df1259651479b3c411e475326a3d9e9002e1f
kwangminini <rhkdals7362@gmail.com>
2020-08-04 00:15:00 +0900

    normal

--- BAEKJOON/stringProblems/1316.py
+++ BAEKJOON/stringProblems/1316.py
@@ -2,17 +2,19 @@ testNum = int(input())
 result=0
 check=[]
 def checkGroup(word):
-    flag=True
+    flag = True
     global result
-    for i in range(len(word)):
+    for i in range(len(word)-1):
         check.append(word[i])
-        if (word[i] in check) and (check[-1] != word[i]):
+
+        if ((word[i+1] in check) and (check[-1] != word[i+1])):
             flag=False
+
     if flag:
         result+=1
 
 
 for _ in range(testNum):
     checkGroup(input())
-    print(result)
     check=[]
+print(result)
