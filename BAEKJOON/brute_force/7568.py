25a9edc23379be84d2e2537e16ebea0bbc924d50
kwangminini <rhkdals7362@gmail.com>
2020-08-12 00:57:29 +0900

    hard

--- BAEKJOON/brute_force/7568.py
+++ BAEKJOON/brute_force/7568.py
@@ -1,6 +1,26 @@
 n = int(input())
-weightList=list()
+weight=list()
+result = list()
 for _ in range(n):
-    weightList.append(list(map(int,input().split())))
-weightList.sort(key=lambda x:(x[0],x[1]),reverse=True)
-print(weightList)
+    weight.append(list(map(int,input().split())))
+#weightList.sort(key=lambda x:(x[0],x[1]),reverse=True)
+weightList=sorted(weight,key=lambda x:(x[0],x[1]),reverse=True)
+num=1
+count=1
+for i in range(len(weightList)-1):
+    if (weightList[i][0] > weightList[i+1][0]) and (weightList[i][1] > weightList[i+1][1]):
+        result.append((weightList[i],num))
+        num = count
+        num+=1
+    else:
+        result.append((weightList[i],num))
+    count+=1
+
+result.append((weightList[-1],num))
+resultList=list()
+for i in range(len(weight)):
+    for j in range(len(result)):
+        if weight[i] == result[j][0]:
+            resultList.append(result[j][1])
+for i in resultList:
+    print(i, end=" ")
