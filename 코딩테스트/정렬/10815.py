01b551efbf6af6d2f11e2d6f1368c8e81e7499d0
kwangminini <rhkdals7362@gmail.com>
2020-10-07 00:40:56 +0900

    숫자카드-정렬-easy

--- 코딩테스트/정렬/10815.py
+++ 코딩테스트/정렬/10815.py
@@ -1,1 +1,12 @@
 n = int(input())
+numberCard = list(map(int,input().split()))
+numberDict = {}
+for i in numberCard:
+    numberDict[i]=1
+m = int(input())
+mList = list(map(int,input().split()))
+for i in mList:
+    if i in numberDict.keys():
+        print(1,end=" ")
+    else:
+        print(0, end=" ")
