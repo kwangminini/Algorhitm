a38af2f787cc9a93a956bade846ac29f958acf9c
kwangminini <rhkdals7362@gmail.com>
2023-02-12 17:50:35 +0900

    feat: solve again programmers 명예의전당(1)

--- 프로그래머스/명예의전당(1).js
+++ 프로그래머스/명예의전당(1).js
@@ -2,18 +2,14 @@ function solution(k, score) {
   let answer = [];
   let queue = [];
   score.map((_score) => {
-    if (queue.length <= k) {
-      queue = queue.concat(_score);
-      queue.sort();
+    if (queue.length < k) {
+      queue = queue.concat(_score).sort((a, b) => a - b);
       answer = answer.concat(queue[0]);
     } else {
-      console.log("queue ## ", queue);
-      // console.log("_score :: ", _score);
-      if (queue[0] < _score) {
-        queue = queue.concat(_score);
-        // console.log("queue ## ", queue);
-        queue.sort();
-        answer = answer.concat(queue.shift());
+      if (queue[0] <= _score) {
+        queue = queue.concat(_score).sort((a, b) => a - b);
+        queue.shift();
+        answer = answer.concat(queue[0]);
       } else {
         answer = answer.concat(queue[0]);
       }
