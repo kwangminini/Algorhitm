b7c2c94db5c60d6c3951c025e6ace14c645993ae
kwangminini <rhkdals7362@gmail.com>
2023-05-28 12:17:31 +0900

    백준알고리즘: 주유소(실버3) BigInt

--- BAEKJOON/greedy/13305.js
+++ BAEKJOON/greedy/13305.js
@@ -1,21 +1,14 @@
-// const fs = require("fs");
-// const input = fs.readFileSync("/dev/stdin").toString().split("\n");
-const n = 4;
-const liter = [3, 3, 4];
-const price = [1, 1, 1, 1];
-// const n = Number(input[0]);
-// const liter = input[1].split(" ").map(Number);
-// const price = input[2].split(" ").map(Number);
-let count = 0;
-let selectPrice = price[0];
-let result = 0;
-for (let i = 0; i < price.length - 1; i++) {
-  if (selectPrice >= price[i + 1]) {
-    result += selectPrice * liter[i];
-    selectPrice = price[i + 1];
-  } else {
-    result += selectPrice * liter[i];
-  }
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().split("\n");
+const n = Number(input[0]);
+const liter = input[1].split(" ").map(Number);
+const price = input[2].split(" ").map(Number);
+
+let minPrice = price[0];
+let result = BigInt(0);
+for (let i = 0; i < n - 1; i++) {
+  minPrice = Math.min(minPrice, price[i]);
+  result += BigInt(minPrice) * BigInt(liter[i]);
 }
 
-console.log(result);
+console.log(String(result));
