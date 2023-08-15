f89886c9fd10a1a42ba5e41146ec1491aa07b5ee
kwangminini <rhkdals7362@gmail.com>
2023-08-15 18:41:05 +0900

    feat: 프로그래머스 소수만들기(LV1)

---
+++ 프로그래머스/Lv1/소수만들기.js
@@ -0,0 +1,27 @@
+function solution(nums) {
+  var answer = 0;
+
+  for (let i = 0; i < nums.length - 2; i++) {
+    for (let j = i + 1; j < nums.length - 1; j++) {
+      for (let k = j + 1; k < nums.length; k++) {
+        if (isPrime(nums[i] + nums[j] + nums[k])) {
+          answer += 1;
+        }
+      }
+    }
+  }
+  return answer;
+}
+
+function isPrime(num) {
+  if (num === 2) {
+    return true;
+  }
+
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
